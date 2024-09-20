d3a0b8c0861823688b649ebdf2790ae7950b1478
farhan ali <farhaneditz24525@gmail.com>
2024-09-20 07:08:13 -0700

    project footer 5

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-green-300 circle-xl p-8 md:p-0 dark:bg-yellow-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/h.jpg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/f.JPG" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        add your home description.
+        Hi I am Farhan,a class 12th student in Khawaja fareed college.I work as a freelancer,taking on various projects and tasks to gain new skills and experiences.
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          home
+          Farhan Ali
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          muslim colony,RYK
+          Freelancer, GKFC
         </div>
       </figcaption>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-green-300 circle-xl p-8 md:p-0 dark:bg-yellow-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/h.jpg" alt="" width="384" height="512"/>
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:circle-none circle-full mx-auto" src="/c.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        add your home description.
+        Phone No.03106982611 Email address:farhaneditz24525@gmail.com.
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-          home
+          Contact
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          muslim colony,RYK
+          Whatsapp,+92 310 6982611
         </div>
       </figcaption>
     </div>
